44c3302e6200552780605daa9b465d34cd4ba926
Moilanen Arto <arto.moilanen@kao.fi>
2024-01-24 23:28:20 +0200

    Touch added. Some minor testings with gyroscope movement.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float gravity = 9.81f;
+    public float force = 9.81f;
     public bool bCollided = false;
     float smooth = 5.0f;
     float tiltAngle = 60.0f;
+    private GameObject DebugText;
+    public float RotationThreshold = 1.635f;
+    float xRot, yRot;
 
     // Start is called before the first frame update
     void Start()
     {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
         Input.gyro.enabled = true;
 
         rb = GetComponent<Rigidbody>();
         bCollided = false;
+
+        DebugText = GameObject.FindGameObjectWithTag("DebugRotation"); 
+        DebugText.GetComponent<TextMeshProUGUI>().text = "";
+
     }
 
     // Update is called once per frame
@@ -24,7 +33,8 @@ public class Movement : MonoBehaviour
     {
         if (!bCollided)
         {
-            /*// Smoothly tilts a transform towards a target rotation.
+            /*
+            // Smoothly tilts a transform towards a target rotation.
             float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
             float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
 
@@ -34,14 +44,30 @@ public class Movement : MonoBehaviour
             // Dampen towards the target rotation
             transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);*/
 
-            transform.rotation = Input.gyro.attitude;
+            //transform.rotation = Input.gyro.attitude;
+            transform.Rotate(-Input.gyro.rotationRateUnbiased.x, Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z,Space.Self);
 
-            rb.AddForce(transform.up * -gravity);
+            /*if (transform.rotation.x > RotationThreshold || 
+                transform.rotation.y > RotationThreshold || 
+                transform.rotation.x < -RotationThreshold || 
+                transform.rotation.y < -RotationThreshold)
+
+            {*/
+                rb.AddForce(-transform.up * force * Time.deltaTime);
+            //}
+
+            if(Input.touchCount > 0 && rb.velocity.y < 2)
+            {
+                Debug.Log("KeyDown");
+
+                rb.AddForce(transform.up * force * 20 * Time.deltaTime);
+            }
+            DebugText.GetComponent<TextMeshProUGUI>().text = new string("gyroX: " + Input.gyro.rotationRateUnbiased.x * 100 + " gyroy: " +Input.gyro.rotationRateUnbiased.y * 100 + " gyroz: " + Input.gyro.rotationRateUnbiased.z * 100 + "\n\n x:" +transform.rotation.x * 100 + " y:" + transform.rotation.y * 100 + " z:" + transform.rotation.z * 100 + "\n\n x:" + rb.velocity.x.ToString() + " y:" + rb.velocity.y.ToString() + " z:" + rb.velocity.z.ToString());
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collided");
+        bCollided = true;
     }
 }
